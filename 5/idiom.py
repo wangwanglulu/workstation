f9b1798bcc32343fd689820acabbb6a67cb45a26
wangwanglulu <177379860@qq.com>
2022-03-21 12:03:46 +0800

    Update idiom.py

--- 5/idiom.py
+++ 5/idiom.py
@@ -1,7 +1,7 @@
 import unicodedata
 
 # 1. 加载成语词典
-filename = 'idiom.txt'
+filename = 'idiom_dictionary.txt'
 with open(filename, encoding="utf-8") as file_object:
     lines = file_object.readlines() #List
 
