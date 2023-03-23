acb4b33a75e4c6c776738e41262b0f7a3452f9a2
wangwanglulu <177379860@qq.com>
2023-03-23 19:48:02 +0800

    lecture 6

--- lecture6/game_func.py
+++ lecture6/game_func.py
@@ -3,7 +3,7 @@ def game(story, goto):
     for sentence in story:
         print(sentence)
         input()
-    select = int(input("Please select?"))
+    select = int(input("Please select? "))
     if select == 1:
         next_stage = goto[0]
     elif select == 2:
@@ -20,7 +20,7 @@ def start(t, m):
     print("Game over")
 
 
-game_t = [[], ["hello"], ["continue"], ["end"]]
+game_t = [[], ['a', 'b', 'c'], ['d', 'e'], ['x', 'y', 'z']]
 game_m = [[], [0, 2, 0], [0, 3, 0], [0, 0, 0]]
 
 start(game_t, game_m)
