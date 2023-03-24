82b328f4014f5fcf7676f5db3666695b8c2ce4c1
wangwanglulu <177379860@qq.com>
2023-03-24 13:00:59 +0800

    ex

--- lecture7/2ex.py
+++ lecture7/2ex.py
@@ -1,17 +1,27 @@
 
 name = "lzl"
+
+
 def f1():
     name = "Eric"
+
     def f2():
         name = "Snor"
         print(name)
     f2()
+
+
 f1()
 
 name = "lzl"
+
+
 def f1():
     name = "Eric"
+
     def f2():
         print(name)
     f2()
+
+
 f1()
--- lecture7/2ex3.py
+++ lecture7/2ex3.py
@@ -1,20 +1,30 @@
 
 name = "lzl"
+
+
 def f1():
     name = "Eric"
+
     def f2():
         nonlocal name
         name = "Snor"
         print(name)
     f2()
+
+
 f1()
 
 name = "lzl"
+
+
 def f1():
     name = "Eric"
+
     def f2():
-    	global name
-    	name = "Snor"
+        global name
+        name = "Snor"
     f2()
     print(name)
+
+
 f1()
