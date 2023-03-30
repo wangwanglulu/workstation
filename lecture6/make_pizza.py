b8e15053e2090fb25b9e440ad96db0ed3b7b9bff
wangwanglulu <177379860@qq.com>
2023-03-30 10:33:32 +0800

    button

---
+++ lecture6/make_pizza.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 29 09:12:28 2023
+
+@author: wangwanglulu
+"""
+import sys
+print(sys.path)
+
---
+++ lecture6/pizza.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 29 09:11:32 2023
+
+@author: wangwanglulu
+"""
+
+def make_pizza(size):
+    print(f"Making a {size}-inch pizza")
