217b8788279a818148a172b3268383493030d117
wangwanglulu <177379860@qq.com>
2023-05-18 21:47:25 +0800

    lecture14

--- lecture12/test.py
+++ lecture12/test.py
@@ -1,38 +1,24 @@
-import requests
-
-from plotly.graph_objs import Bar
-from plotly import offline
-
-URL = 'https://api.github.com/search/repositories?q=language:python&sort=star'
-r = requests.get(URL)
-print("Status code:", r.status_code)
-response_dict = r.json()
-
-repo_dicts = response_dict['items']
-names, stars = [], []
-for repo_dict in repo_dicts:
-    names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
-
-data = [{
-    'type': 'barh', 'x': names,'y': stars,
-    'marker': {'color': 'rgb(60, 100, 150)',
-               'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
-    },
-    'opacity': 0.6,
-}]
-
-my_layout = {
-    'title': 'Most-Starred Python Projects on GitHub',
-    'titlefont': {'size': 28},
-    'xaxis': {'title': 'Repository',
-              'titlefont': {'size': 24},
-              'tickfont': {'size': 14},
-    },
-    'yaxis': {'title': 'Stars',
-              'titlefont': {'size': 24},
-              'tickfont': {'size': 14},
-    },
-}
-fig = {'data': data, 'layout': my_layout}
-offline.plot(fig, filename='python_repos.html')
+import json
+filename = 'btc_close_2017.json'
+with open(filename) as f:
+    btc_data = json.load(f)  
+date=[]; close=[]; months=[]
+for btc_dict in btc_data:
+    date.append(btc_dict['date'])
+    months.append(int(btc_dict['month']))
+    close.append(int(float(btc_dict['close'])))
+from itertools import groupby
+import matplotlib.pyplot as plt
+xy_map = []
+for x, y in groupby(sorted(zip(months, close)), lambda w: w[0]):  
+    y_list = []
+    for first, second in y:
+        y_list.append(second)
+    xy_map.append([x, sum(y_list) / len(y_list)])  
+    x_unique, y_mean = zip(*xy_map) 
+fig, ax = plt.subplots()
+plt.style.use('seaborn')
+ax.plot(x_unique, y_mean, linewidth=1)
+ax.scatter(x_unique, y_mean, s=20)
+ax.set_title('Close',fontsize=10)
+plt.savefig('close.jpg',dpi=300)
