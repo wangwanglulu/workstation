226bc5b747ac16d252d5de387ce4bb74666507d3
wangwanglulu <177379860@qq.com>
2023-05-24 10:56:47 +0800

    try

--- lecture13/IMDB.py
+++ lecture13/IMDB.py
@@ -2,11 +2,21 @@ import requests
 
 api_key = 'k_kvjck39o'
 
-url=f"https://imdb-api.com/en/API/Top250Movies/{api_key}"
+url=f"https://imdb-api.com/en/API/InTheaters/{api_key}"
 r = requests.get(url)
 print(r.status_code)
 response_dict = r.json() 
-top250 = response_dict["items"]
-print(top250[0])
-
-
+ 
+in_theaters = response_dict["items"]
+ 
+for movie in in_theaters:
+    url = movie['image']
+    title = movie['title']
+    print(title)
+ 
+    r = requests.get(url)
+    if r.status_code == 200:
+        with open(f'{title}.jpg', 'wb') as f:
+            f.write(r.content)
+    else:
+        print("download failed")
