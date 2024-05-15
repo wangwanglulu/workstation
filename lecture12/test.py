2d5d4052a715a1d3e05c0943c866b133815eca78
wangwanglulu <177379860@qq.com>
2024-05-15 17:14:34 +0800

    lecture 12

--- lecture12/test.py
+++ lecture12/test.py
@@ -1,24 +1,26 @@
+from pathlib import Path
+from datetime import datetime
 import json
-filename = 'btc_close_2017.json'
-with open(filename) as f:
-    btc_data = json.load(f)  
+
+path = Path('btc_close_2017.json')
+contents = path.read_text()
+btc_data = json.loads(contents)
+
 date=[]; close=[]; months=[]
+
 for btc_dict in btc_data:
-    date.append(btc_dict['date'])
+    date.append(datetime.strptime(btc_dict['date'], "%Y-%m-%d"))
     months.append(int(btc_dict['month']))
     close.append(int(float(btc_dict['close'])))
-from itertools import groupby
+
 import matplotlib.pyplot as plt
-xy_map = []
-for x, y in groupby(sorted(zip(months, close)), lambda w: w[0]):  
-    y_list = []
-    for first, second in y:
-        y_list.append(second)
-    xy_map.append([x, sum(y_list) / len(y_list)])  
-    x_unique, y_mean = zip(*xy_map) 
-fig, ax = plt.subplots()
+
 plt.style.use('seaborn')
-ax.plot(x_unique, y_mean, linewidth=1)
-ax.scatter(x_unique, y_mean, s=20)
+fig, ax = plt.subplots()
+
+ax.plot(date,close, linewidth=0.5, c='red')
+ax.scatter(date,close, s=5, c='red')
 ax.set_title('Close',fontsize=10)
+fig.autofmt_xdate()
+
 plt.savefig('close.jpg',dpi=300)
