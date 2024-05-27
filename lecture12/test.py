4772e5936a3b18e3021de491967518ab1f10b3e0
wangwanglulu <177379860@qq.com>
2024-05-27 16:46:32 +0800

    case study

--- lecture12/test.py
+++ lecture12/test.py
@@ -1,4 +1,21 @@
-from datetime import datetime
-first_date = datetime.strptime('2021-7-1', '%Y-%m-%d') 
-second_date = datetime.strptime('2022-9-1', '%Y-%m-%d') 
-print(second_date-first_date)
+import matplotlib.pyplot as plt
+   
+def threepoints(z):
+    x1=z[0][0]
+    y1=z[0][1]
+    x2=z[1][0]
+    y2=z[1][1]
+    y=z[2][1]
+    x=z[2][0]
+    if (x2-x1)*y-(y2-y1)*x-y1*x2+y2*x1==0:
+       print('They lie on a straight line.')
+    else:
+        plt.plot([z[0][0],z[1][0]],[z[0][1],z[1][1]],c="black")
+        plt.plot([z[1][0],z[2][0]],[z[1][1],z[2][1]],c="black")
+        plt.plot([z[0][0],z[2][0]],[z[0][1],z[2][1]],c="black")
+        plt.show()
+
+n=[[1,1],[1,2],[1,3]]
+m=[[1,1],[2,3],[4,2]]
+threepoints(n)
+threepoints(m)
