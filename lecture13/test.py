2162c3714e7fea5d787b788047bc7ff6f6faf768
wangwanglulu <177379860@qq.com>
2024-05-27 17:48:34 +0800

    lecture 13

--- lecture13/test.py
+++ lecture13/test.py
@@ -1,17 +1,26 @@
 import requests
-url = "http://t.weather.itboy.net/api/weather/city/101020100"
+import matplotlib.pyplot as plt
+url="http://img1.money.126.net/data/hs/kline/day/history/2022/0600519.json"
 r = requests.get(url)
 print(r.status_code)
-response_dict = r.json()  
-f = response_dict['data']
-ff = f['forecast']
-ff_today = ff[0]
-ff_1 = ff[1]
-ff_2 = ff[2]
-def show(day):
-    for x in day:
-        print(x+': '+str(day[x]))
-    print()
-show(ff_today)
-show(ff_1)
-show(ff_2)
+response_dict = r.json() 
+print(response_dict)
+
+data = response_dict['data']
+for x in data[:5]:
+    print("""日期: {}，开盘价：{}，收盘价：{}，最高价：{}
+        最低价：{}，交易量：{}，涨幅跌幅：{}""".format(x[0],\
+        x[1], x[2], x[3], x[4], x[5], x[6]))
+
+from datetime import datetime
+
+date, close_p = [], []
+for y in data:
+    date.append(datetime.strptime(y[0],"%Y%m%d"))
+    close_p.append(y[2])
+
+fig, ax = plt.subplots()
+ax.plot(date,close_p, linewidth=0.5)
+ax.scatter(date,close_p, s=5)
+fig.autofmt_xdate()
+plt.savefig('maotai.jpg',dpi=300)
