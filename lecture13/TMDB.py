fe480234389b725829d67b83123ef295f013c270
wangwanglulu <177379860@qq.com>
2024-11-24 09:35:35 +0800

    update web api

--- lecture13/TMDB.py
+++ lecture13/TMDB.py
@@ -1,26 +1,28 @@
 import requests
 from pathlib import Path
 
-api_access = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiN2JmNTExZGFmM2ZjMDY4NGEzNTQxNWJjM2MzZmZmZSIsInN1YiI6IjVkNTBmMDk5MDEwMmM5Mjc2ZDc0ZTI5OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.n48HcsjdiXRSY4Xs8qFRtvy50P2W1sppZ5OaAMU_Rd0"
+api_access = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZWMzMDlmYWZjMjM4NjdmZmViNzRjMjZjN2E4YTU3OSIsIm5iZiI6MTczMjQxMTUwMy40OTM0MjMsInN1YiI6IjVkNTBmMDk5MDEwMmM5Mjc2ZDc0ZTI5OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.i5Gpj1yc_9I7JarSzgoBcI2uWKOd3W0wiftwwOIFYak"
 
 page = 1
-url = f"https://api.themoviedb.org/3/movie/\
-top_rated?language=en-US&page={page}"
+url = f"https://api.tmdb.org/3/movie/\
+now_playing?language=en-US&page={page}" #改成api.tmdb.org
 headers = {
     "accept": "application/json",
     "Authorization": f"Bearer {api_access}"
 }
 response = requests.get(url, headers=headers)
 response_dict = response.json()
+ 
 movies=response_dict["results"]
 top10 = movies[:10]
+
 for movie in top10:
     poster = movie['poster_path']
     title = movie['title']
     img_url = f"https://image.tmdb.org/t/p/w500{poster}"
     r = requests.get(img_url, headers=headers)
     if r.status_code == 200:
-        save_path = Path(f"{title}.jpg")
-        save_path.write_bytes(r.content)
+        with open(f'{title}.jpg', 'wb') as f:
+            f.write(r.content)
     else:
         print("download failed")
