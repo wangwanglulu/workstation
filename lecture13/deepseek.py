2665184d54a9982cca6b3ea5ce4e29c2c85d40ac
wangwanglulu <177379860@qq.com>
2025-05-16 16:26:01 +0800

    lecture 13 2025

---
+++ lecture13/deepseek.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 17 12:58:16 2023
+
+@author: wangwanglulu
+"""
+
+# Please install OpenAI SDK first: `pip3 install openai`
+
+from openai import OpenAI
+
+client = OpenAI(api_key="sk-e4da7a6207fe4343b2db3de0de40ed57", base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "鲁迅暴打周树人"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
+
