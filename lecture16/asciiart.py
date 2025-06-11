6a6cf7bb958c35893babd895bba0dd2b41bb8fe6
wangwanglulu <177379860@qq.com>
2025-06-11 11:01:56 +0800

    ascii art 2025

--- lecture16/asciiart.py
+++ lecture16/asciiart.py
@@ -1,6 +1,6 @@
-from moviepy.editor import *
+from moviepy import *
 clip = VideoFileClip("ultraman.flv")
-clip = clip.subclip(0, 6) #剪切前8秒
+clip = clip.subclipped(0, 6) #剪切前6秒
 clip.write_gif("video.gif") #保存为gif
 
 import os
