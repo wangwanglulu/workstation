e16817018d59608e7f7f00dbda3e191356ddf354
wangwanglulu <177379860@qq.com>
2025-10-30 15:52:45 +0800

    mba2025

--- lecture12/test.py
+++ lecture12/test.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import csv
- 
+
 file_path = Path("sitka_weather_07-2021_simple.csv")
 with file_path.open("r", encoding="utf-8") as file:  # 指定编码
     csv_reader = csv.reader(file)
