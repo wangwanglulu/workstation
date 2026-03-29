ba270ab714c4c4ea7a65092ba2f51c6af7b74324
wangwanglulu <177379860@qq.com>
2026-03-29 22:47:06 +0800

    lecture 5 2026

--- lecture5/test.py
+++ lecture5/test.py
@@ -1,30 +1,30 @@
+from pathlib import Path
+path = Path('comment.txt')
+contents = path.read_text()
+
+words = ["bad", "boring", "awful", "terrible"]
 
 
-# 1. 加载成语词典
-filename = 'idiom_dictionary.txt'
-with open(filename, encoding="utf-8") as file_object:
-    lines = file_object.readlines()  # List
-    
-# 谐音取词
-# 修改第一步
-import unicodedata
-d_game={}
-for line in lines:
-    if line!="\n":
-        endpoint=line.find("拼音")
-        idiom=line[:endpoint].strip()
-        pinyin_start=line.find("：", endpoint)
-        pinyin_end=line.find("释义")
-        each=line[pinyin_start+1: pinyin_end]
-        each=unicodedata.normalize('NFKD',each).encode('ascii','ignore').decode()
-        pinyin_list=each.split()
-        d_game[idiom]=pinyin_list
+#转化小写
+text = contents.lower()
 
+# 去除标点（只保留字母和空格）
+cleaned_text = ""
+for char in text:
+    if char.isalpha() or char.isspace():
+        cleaned_text += char # cleaned_text = cleaned_text + char
 
-# 2. 给定一个成语，找到可以接上的所有成语
-idiom = input("请输入第一个成语\n")
-char_4th = d_game[idiom][-1]
-for x, y in d_game.items():
-    if char_4th == y[0]:
-        print(x)
+d = {}
+total = 0
+cleaned_text = cleaned_text.split()
+
+for c in cleaned_text:
+    d[c] = d.get(c,0)+1
+for word in words:
+    if word in d:
+        total = d[word] + total
+
+print(d)
+print(total)
+
 
