fba0fc2a8f4fd44151e306737c23a11fb08ad982
wangwanglulu <177379860@qq.com>
2026-04-27 09:45:21 +0800

    lecture 9 2026

--- lecture7/test.py
+++ lecture7/test.py
@@ -4,16 +4,19 @@ Created on Sat Apr 11 19:38:13 2026
 
 @author: Lu
 """
-import time
-from playsound import playsound
+import random
+ranks = list(map(str, [value for value in range(2, 15)]))
+suits = ["\u2660", "\u2665", "\u2663", "\u2666"]
+deck = [y + x for x in ranks for y in suits]
 
-def countdown(seconds):
-    while seconds >= 0:
-        print(f"\r剩余：{seconds:02d} 秒", end="", flush=True)
-        time.sleep(1)
-        seconds -= 1
+random.shuffle(deck)
 
-    print("\n时间到！")
-    playsound("dismiss.mp3")
-countdown(3)
-
+# 简化处理，一次性发了7张
+hand = [deck.pop() for x in range(7)]
+print(hand)
+all = [c[0] for c in hand]
+max_suits = [all.count(x) for x in suits]
+if max(max_suits) >=5:
+    print("Yes")
+else:
+    print("No")
